a631bc1f96c7e316f4d70c96f13c5b6a161fff25
Brad Mehder <bmehder@gmail.com>
2021-02-10 12:52:01 -0600

    last night's refactoring

--- recorder.js
+++ recorder.js
@@ -1,32 +1,30 @@
-const handleData = (mediaStream) => {
-  console.log('The mic is open.');
-
+const handle = (mediaStream) => {
   const recorder = new MediaRecorder(mediaStream);
   const recording = [];
 
-  document
-    .querySelector('#start-audio')
-    .addEventListener('click', () => recordForSeconds(3));
-
-  const recordForSeconds = (seconds) => {
+  const record = () => {
+    let seconds = document.querySelector('#seconds').value || 3;
     recorder.start();
     setTimeout(() => recorder.stop(), seconds * 1000);
   };
+
+  const makeURL = (recording) => {
+    const blob = new Blob(recording, { type: 'audio/wav' });
+    const url = URL.createObjectURL(blob);
+    console.log(`The file can be fetched from ${url}`);
+  };
+
+  document
+    .querySelector('#start-audio')
+    .addEventListener('click', () => record());
 
   recorder.addEventListener('dataavailable', (e) => {
     recording.push(e.data);
     recorder.state === 'inactive' && makeURL(recording);
   });
-
-  const makeURL = (recording) => {
-    const blob = new Blob(recording, { type: 'audio/wav' });
-    const url = URL.createObjectURL(blob);
-    console.log(`The wav file can be fetched from ${url}`);
-  };
 };
 
-navigator.mediaDevices &&
-  navigator.mediaDevices
-    .getUserMedia({ audio: true })
-    .then((data) => handleData(data))
-    .catch((err) => console.log("Don't panic, but " + err.message));
+navigator.mediaDevices
+  .getUserMedia({ audio: true })
+  .then((mediaStream) => handle(mediaStream))
+  .catch((err) => console.log("Don't panic, but " + err.message));
